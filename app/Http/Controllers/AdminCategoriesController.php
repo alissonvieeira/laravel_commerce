5921cab81a1ba31403cf1b87ba05264002967c0b
Alisson Vieira <alisson.echo@gmail.com>
2016-10-13 01:25:12 -0300

    Adicionando models, rotas, controllers de Category e Product

---
+++ app/Http/Controllers/AdminCategoriesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CodeCommerce\Http\Controllers;
+
+use CodeCommerce\Category;
+use Illuminate\Http\Request;
+
+use CodeCommerce\Http\Requests;
+use CodeCommerce\Http\Controllers\Controller;
+
+class AdminCategoriesController extends Controller
+{
+    public function index()
+    {
+        dd(Category::all());
+    }
+}
---
+++ app/Http/Controllers/AdminProductsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CodeCommerce\Http\Controllers;
+
+use CodeCommerce\Product;
+use Illuminate\Http\Request;
+
+use CodeCommerce\Http\Requests;
+use CodeCommerce\Http\Controllers\Controller;
+
+class AdminProductsController extends Controller
+{
+    public function index()
+    {
+        dd(Product::all());
+    }
+}
