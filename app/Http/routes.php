35b234a4faea2c0c78629d07c99041cfcbb6d4d5
Alisson Vieira <alisson.echo@gmail.com>
2016-10-14 00:17:20 -0300

    Trabalhando com parâmetros

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,5 +4,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::pattern('id', '[0-9]+');
+
+Route::get('user/{id?}', function ($id =123) {
+    if($id)
+        return "Olá $id";
+
+    return "Não possui ID";
+});
+
+
+
 Route::get('admin/categories', 'AdminCategoriesController@index');
 Route::get('admin/products', 'AdminProductsController@index');
