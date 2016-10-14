a0ed315977315f81dccd1e524f4dad4e914e4948
Alisson Vieira <alisson.echo@gmail.com>
2016-10-14 00:26:27 -0300

    Names routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,6 +4,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('produtos', ['as' => 'produtos', function(){
+    echo Route::currentRouteName();
+    //return "Produtos";
+}]);
+
+
+
+
 Route::pattern('id', '[0-9]+');
 
 Route::get('user/{id?}', function ($id =123) {
