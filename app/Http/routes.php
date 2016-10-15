7f712440d0ba50912626bed9d3fba4c8e08dffd6
Alisson Vieira <alisson.echo@gmail.com>
2016-10-14 23:02:25 -0300

    Criando controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,9 @@
 <?php
 
+Route::get('categories', 'CategoriesController@index');
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -9,27 +13,6 @@ Route::pattern('id', '[0-9]+');
 Route::group(['prefix' => 'admin'], function(){
 
     Route::get('categories', 'AdminCategoriesController@index');
-    Route::get('products', 'AdminProductsController');
+    Route::get('products', 'AdminProductsController@index');
 
 });
-
-
-Route::get('category/{category}', function(\CodeCommerce\Category $category){
-    return $category->name;
-});
-
-
-
-Route::get('user/{id?}', function ($id =123) {
-    if($id)
-        return "Olá $id";
-
-    return "Não possui ID";
-});
-
-
-
-
-
-Route::get('admin/categories', 'AdminCategoriesController@index');
-Route::get('admin/products', 'AdminProductsController@index');
