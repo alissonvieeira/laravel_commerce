e3f61a0060ec98a2093ebaab09184681c9ed16db
Alisson Vieira <alisson.echo@gmail.com>
2016-10-21 20:06:02 -0200

    Criando StoreController

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,18 +28,4 @@ Route::group(['prefix' => 'admin', 'where' => ['id' => '[0-9]+']], function()
     });
 });
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-/*
-Route::pattern('id', '[0-9]+');
-
-Route::group(['prefix' => 'admin'], function(){
-
-    Route::get('categories', 'AdminCategoriesController@index');
-    Route::get('products', 'AdminProductsController@index');
-
-});
-*/
+Route::get('/', 'StoreController@index');
