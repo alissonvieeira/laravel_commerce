7be5c7914f4dda10db5a542c6a37a8f86d41ffad
Alisson Vieira <alisson.echo@gmail.com>
2016-10-22 02:00:40 -0200

    Exibindo Produtos Recomendados

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -3,6 +3,7 @@
 namespace CodeCommerce\Http\Controllers;
 
 use CodeCommerce\Category;
+use CodeCommerce\Product;
 use Illuminate\Http\Request;
 
 use CodeCommerce\Http\Requests;
@@ -12,8 +13,10 @@ class StoreController extends Controller
 {
     public function index()
     {
+        $pFeatured = Product::where('featured','=',1)->get();
+
         $categories = Category::all();
 
-        return view('store.index', compact('categories'));
+        return view('store.index', compact('categories', 'pFeatured'));
     }
 }
