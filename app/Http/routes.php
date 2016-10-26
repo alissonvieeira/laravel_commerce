dc26a431423146514b8b6338c1b931240e11677b
Alisson Vieira <alisson.echo@gmail.com>
2016-10-26 20:51:14 -0200

    Controller Cart

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,3 +31,4 @@ Route::group(['prefix' => 'admin', 'where' => ['id' => '[0-9]+']], function()
 Route::get('/', ['as' => 'store.index', 'uses' => 'StoreController@index']);
 Route::get('category/{id}', ['as' => 'store.category', 'uses' => 'StoreController@category']);
 Route::get('product/{id}', ['as' => 'store.product', 'uses' => 'StoreController@product']);
+Route::get('cart', ['as' => 'cart', 'uses' => 'CartController@index']);
