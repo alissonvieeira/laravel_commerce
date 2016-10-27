b0db9d9cecd3d654b15ad25710f41432c838354d
Alisson Vieira <alisson.echo@gmail.com>
2016-10-27 01:30:42 -0200

    View Cart

--- resources/views/store/cart.blade.php
+++ resources/views/store/cart.blade.php
@@ -1,5 +1,49 @@
 @extends('store.store')
 
 @section('content')
-    Carrinho
+    <section id="cart_items">
+        <div class="container">
+            <div class="table-responsive cart_info">
+                <table class="table table-condensed">
+                    <thead>
+                        <tr class="cart_menu">
+                            <td class="image">Item</td>
+                            <td class="description"></td>
+                            <td class="price">Valor</td>
+                            <td class="price">Quantidade</td>
+                            <td class="price">Total</td>
+                            <td></td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($cart->all() as $k=>$item)
+                        <tr>
+                            <td class="cart_product">
+                                <a href="#">
+                                    Imagem
+                                </a>
+                            </td>
+                            <td class="cart_description">
+                                <h4><a href="#">{{ $item['name'] }}</a></h4>
+                                <p>Código: {{ $k }}</p>
+                            </td>
+                            <td class="cart_price">
+                                R$ {{ $item['price'] }}
+                            </td>
+                            <td class="cart_quantity">
+                                {{ $item['qtd'] }}
+                            </td>
+                            <td class="cart_total">
+                                <p class="cart_total_price">R$ {{ $item['price'] * $item['qtd'] }}</p>
+                            </td>
+                            <td class="cart_delete">
+                                <a href="#" class="cart_quantity_delete">Delete</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
 @stop
